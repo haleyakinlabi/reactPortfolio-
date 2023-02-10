4eda352b7f1ad8468291cf6700388f87e6fe1bdc
Haley Akin <107583062+wasatchtimpanogos@users.noreply.github.com>
2023-02-10 14:59:59 -0700

    Create Contact.jsx

---
+++ src/scenes/Contact.jsx
@@ -0,0 +1,85 @@
+import LineGradient from "../components/LineGradient";
+import { useForm } from "react-hook-form";
+import { motion } from "framer-motion";
+
+const Contact = () => {
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (e) => {
+    console.log("~ e", e);
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <section id="contact" className="contact py-48">
+      {/* HEADINGS */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, x: 50 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        className="flex justify-end w-full"
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-yellow">CONTACT ME</span> TO GET STARTED
+          </p>
+          <div className="flex md:justify-end my-5">
+            <LineGradient width="w-1/2" />
+          </div>
+        </div>
+      </motion.div>
+
+      {/* FORM & IMAGE */}
+      <div className="md:flex md:justify-between gap-16 mt-5">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          className="basis-1/2 flex justify-center"
+        >
+          <img src="../assets/aiPortrait2.png" alt="contact" />
+        </motion.div>
+
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          className="basis-1/2 mt-10 md:mt-0"
+        >
+          <div>
+            <br></br>
+            <p className="font-playfair font-semibold text-3xl">Email</p>
+            <p>wasatchtimpanogos@outlook.com</p>
+            <br></br>
+            <p className="font-playfair font-semibold text-3xl">Mobile</p>
+            <p>385-277-8646</p>
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
